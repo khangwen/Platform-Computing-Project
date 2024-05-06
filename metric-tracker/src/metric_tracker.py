3af13556211aed3a899556d9f5189134c45814df
Alan Nguyen <a_wen197@yahoo.com>
2024-05-05 21:22:33 -0700

    metric tracker setup to receive new users

--- metric-tracker/src/metric_tracker.py
+++ metric-tracker/src/metric_tracker.py
@@ -4,11 +4,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pymongo
 
+from users.user3 import userAction
+
 # Set up MongoDB
 myclient = pymongo.MongoClient("mongodb+srv://khangwen197:HsUFKqdVgto1Qyo5@cluster0.pqitser.mongodb.net/")
 mydb = myclient["CSE4200"]
 
-mycol = mydb["Metrics"]
+mycol = mydb["Metrics2"]
 
 # Initialize browser
 driver = webdriver.Firefox()
@@ -16,33 +18,25 @@ driver = webdriver.Firefox()
 # Navigate to your website 
 driver.get("http://localhost:3000/")
 
-metrics = []
+# Metric info
+itr = 0
+group = "Control"
+user = "user1"
+
 # Track presence time 
 start_time = time.time()
 presence_time = start_time
 
-# Initialize number of clicks
-num_clicks = 0
+# User action
+userAction(driver)
 
-# Initialize title
-title = driver.title
-
-while True:#presence_time < 50: # seconds
-    now = datetime.datetime.now().strftime("%H:%M:%S")
-
-    current_time = time.time()
-    presence_time = current_time - start_time
-    print(f"Presence time: {presence_time} seconds")
+# Calculate presence time
+current_time = time.time()
+presence_time = current_time - start_time
+print(f"Presence time: {presence_time} seconds")
     
-    # Track scrolling
-    scroll_height = driver.execute_script("return document.body.scrollHeight")  
-    current_scroll = driver.execute_script("return window.pageYOffset")
-    print(f"Scrolled {current_scroll}/{scroll_height} pixels")
-    
-    # Insert data into MongoDB
-    new_entry = {"TIMESTAMP (HH/MM/SS)": now, "PRESENCE_TIME (SEC.)": presence_time, "SCROLLING (PIXELS)": current_scroll}
-    mycol.insert_one(new_entry)
-
-    time.sleep(2) 
+# Insert data into MongoDB
+new_entry = {"Iteration": itr, "Group": group, "User": user, "PRESENCE_TIME (SEC.)": presence_time}
+mycol.insert_one(new_entry)
         
 driver.quit()
