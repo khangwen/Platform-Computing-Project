4c8cae662c38ff77aa63c636eec840b89f1d9ddd
michilcutt <145288129+michilcutt@users.noreply.github.com>
2024-05-06 09:46:26 -0700

    Update App.js
    updating for next iteration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,89 +8,103 @@ import tab from './assets/img/logo.gif';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={button} className="App-logo" alt="logo" />
-      </header>
-      {/*Making a github button*/}
-      <a href="https://github.com/michilcutt/Platform_Computing.git" target="_blank">
-        <button className="Button1">
-          <img src={logo} className="GITHUB-LOGO" width="50" alt="GITHUB"></img>
-        </button>
-      </a>
 
-      {/*First Paragraph*/}
-      <header className="First">
-        <p style={{ color: 'black' }}>
-          <header style={{ color: 'darkcyan', fontWeight: 'bold', fontFamily: 'Franklin Gothic Medium' }}>
-            Nonstop grind? Coffee coded? Me too!
-          </header>
-          <hr />
-          Afro Code is like a DNA Sequence from Jurassic Park, always changing and editing
-          lines of code throughout the different evolutions of themselves. Transferring from a community college
-          to a university was a step that was new, intriguing but frightening. Few things of comfort came through the
-          hard journey: coffee(put emoji here), animals(put emoji here), and new surroundings. Along with
-          these new surroundings, new interests came as well.
-        </p>
-      </header>
-      {/*Spruce it up*/}
-      <img src={divider} className="Divider-Break" alt="break" />
-      <img src={end} className="Pika" alt="Pika Pika" />
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={button} className="App-logo" alt="logo" />
+            </header>
+            {/*Making a github button*/}
+            <a href="https://github.com/khangwen/Platform-Computing-Project.git" target="_blank">
+                <button className="Button1">
+                    <img src={logo} className="GITHUB-LOGO" width="50" alt="GITHUB"></img>
+                </button>
+            </a>
 
-      {/*Second Paragraph*/}
-      <header className="Second">
-        <p style={{ color: 'black' }}>
-          <header style={{ color: 'darkcyan', fontWeight: 'bold', fontFamily: 'Franklin Gothic Medium' }}>
-            Rediscovered Hobbies!
-          </header>
-          <hr />
-          <h2 style={{ color: 'mediumvioletred', fontFamily: 'Chiller', fontWeight: 'bold' }}>Photography</h2>
-          While Photography, isn't necessarly a new interest I've had it only became stronger when I took an actual class
-          here. I always only took photos on my phone and before I started Uni here I was given an old video camera by my pops.
-          It took off from there but once I started taking a class and realized the beauty and bigger picture(ha!) of it all,
-          it was like rediscovering your favorite song from when you were in high school. It's refreshing and suddenly everything is
-          reignited again. Reminds me of my everygrowing passion and fear of my field.
-        </p>
-      </header>
+            {/*First Tester/Blog Entry*/ }
+            <header className="First">
+                <p style={{ color: 'black' }}>
+                    <h1 style={{ color: 'bisque', fontWeight: 'bold', fontFamily:'Copperplate, Papyrus, fantasy' }}>
+                    OuterWorld Delicacies
+                </h1>
+                    <hr />
+                    Wanna taste something otherwordly? Something familar with a fantastical twist? Something you never tried before?
+                    Or do you just enjoy the show you watch and want to replicate or make it better? Well, welcome to OuterWorld
+                    Delicacies! Jump into different worlds, and expand your taste buds to make a boring night into some colorful memories!
+                </p>
+            </header>
 
-      {/*Third Paragraph*/}
-      <header className="Third">
-        <p>
-          <header style={{ color: 'darkcyan', fontWeight: 'bold', fontFamily: 'Franklin Gothic Medium' }}>
-            Max Passion!
-          </header>
-          <hr />
-          <h2 style={{ color: 'palegoldenrod', fontWeight: 'bold', fontFamily: 'Chiller' }}>My Major</h2>
-          My major is in Computer Systems: Game Development Concentration. I want to make video games and I've always wanted to make
-          games since I could remember playing my first game on V.SMILE a game console for kids and while it was for educational purposes
-          I still fell inlove with the comfort and excitement of playing a video game. It is not my end goal to make video games, AstroPhysicist,
-          it's still a passion I adore and fear at the same time. I am always scared that I'll lose my admiration and determination to make
-          games because of the complexity and difficulty of that world in general. But that fear is still driving me to finish this degree
-          and continue my dreams.
-        </p>
-      </header>
 
-      {/*Ending Space*/}
-      <header className="Ending">
-        <footer>
-          &copy;MKC
-        </footer>
-        {/*making favorite song at the moment button link*/}
-        <a href="https://youtu.be/uTMuqL0qx08?si=BnEA0GLLQZW7gW6V" target="_blank" alt="Fave song at the moment">
-          <button className="Button2">
-            <img src={goodbye} className="GoodbyeDivider" width="50" alt="Totoro"></img>
-          </button>
-        </a>
-        {/*making a button link for the page in general*/}
-        <a href="http://localhost:3000/" target="_blank" alt="Homepage">
-          <button className="Button3">
-            <img src={tab} className="GoodbyeDivider2" width="50" alt="Cute Doggo"></img>
-          </button>
-        </a>
-      </header>
-    </div>
+            {/*Spruce it up*/}
+            <img src={divider} className="Divider-Break" alt="break" />
+            <img src={end} className="Pika" alt="Pika Pika" />
 
-  );
+            {/*Second Paragraph*/}
+            <header className="Second">
+                <p style={{ color: 'black' }}>
+                    <h1 style={{ color: 'darkcyan', fontWeight: 'bold', fontFamily: 'Copperplate, Papyrus, fantasy' }}>
+                        Curry From Another World
+                    </h1>
+                    <hr />
+                    <h2 style={{ color:'brown', fontFamily: 'Malgun Gothic', fontWeight: 'bold' }}>Chicken Curry</h2>
+                    Let's start easy! What is something that is easy to make and can be made towards your personal preference? Curry!
+                    You can make curry with any type of protein if you want or simply with just vegetables as well! Today the main focus is
+                    the Chicken Curry from Restaurant to Another World!
+                </p>
+                <hr />
+
+                <h3>Ingredients:</h3>
+                {/*Ingredient List*/ }
+                <p>
+                    1lb boneless, skinless chicken breast <br />
+                    1lb carrots<br />
+                    1 White/Purple Onion<br />
+                    1lb Yukon Gold Potatoes
+                    2 Garlic Cloves<br />
+                    1 tsp Ground Ginger<br />
+                    1/4 cup Olive Oil<br />
+                    1 tsp Ground Cumin<br />
+                    1 tsp Ground Coriander<br />
+                    1 tsp Ground Turmeric<br />
+                    1 tsp Garlic Salt<br />
+                    1 tbsp Soy <br />
+                    1 tbsp <br />
+                    1 tbsp Oyster <br />
+                    6 Cups Water<br />
+                </p>
+            </header>
+
+            {/*Third Paragraph*/}
+            <header className="Third">
+                <p>
+                    <h1 style={{ color: 'darkcyan', fontWeight: 'bold', fontFamily: 'Copperplate, Papyrus, fantasy' }}>
+                        Max Passion!
+                    </h1>
+                    <hr />
+                    <h2 style={{ color: 'brown', fontWeight: 'bold', fontFamily: 'Malgun Gothic'}}>2nd Recipe</h2>
+                </p>
+            </header>
+
+            {/*Ending Space*/}
+            <header className="Ending">
+                <footer>
+                    &copy;OTD
+                </footer>
+                {/*making favorite song at the moment button link*/}
+                <a href="https://youtu.be/uTMuqL0qx08?si=BnEA0GLLQZW7gW6V" target="_blank" alt="Fave song at the moment">
+                    <button>
+                        <img src={goodbye} className="GoodbyeDivider" width="50" alt="Totoro"></img>
+                    </button>
+                </a>
+                {/*making a button link for the page in general*/}
+                <a href="http://localhost:3000/" target="_blank" alt="Homepage">
+                    <button>
+                        <img src={tab} className="GoodbyeDivider2" width="50" alt="Cute Doggo"></img>
+                    </button>
+                </a>
+            </header>
+        </div>
+        
+    );
 }
 export default App;
