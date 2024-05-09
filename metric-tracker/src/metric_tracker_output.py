26cd563d920ae958936385fc4b052e086e21d3b1
Alan Nguyen <a_wen197@yahoo.com>
2024-05-09 11:02:04 -0700

    updated with average calculations

--- metric-tracker/src/metric_tracker_output.py
+++ metric-tracker/src/metric_tracker_output.py
@@ -9,7 +9,7 @@ import pandas as pd
 myclient = pymongo.MongoClient("mongodb+srv://khangwen197:HsUFKqdVgto1Qyo5@cluster0.pqitser.mongodb.net/")
 mydb = myclient["CSE4200"]
 
-mycol = mydb["Metrics3"]
+mycol = mydb["Iteration1_Metrics"]
 
 # Print data from MongoDB as table
 print("\nData from MongoDB:")
@@ -17,3 +17,11 @@ metric_data = [data for data in mycol.find()]
 df_metric_data = pd.DataFrame(metric_data)
 # df_metric_data.to_csv('out.csv', index=False)  
 print(df_metric_data.sort_values(by=["Group", "User"]))
+
+# Calculate average presence time
+control_group = df_metric_data[df_metric_data["Group"] == "Control"]
+test_group = df_metric_data[df_metric_data["Group"] == "Test"]
+control_avg = control_group["PRESENCE_TIME (SEC.)"].mean()
+test_avg = test_group["PRESENCE_TIME (SEC.)"].mean()
+print(f"\nAverage presence time for Control group: {control_avg} seconds")
+print(f"Average presence time for Test group: {test_avg} seconds")
