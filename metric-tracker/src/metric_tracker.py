c77449e7ff4d12f4c385cc122cd2f88d227bd42f
Alan Nguyen <a_wen197@yahoo.com>
2024-05-14 14:38:22 -0700

    prep for final iteration

--- metric-tracker/src/metric_tracker.py
+++ metric-tracker/src/metric_tracker.py
@@ -18,20 +18,20 @@ from users import user54 as user_file54
 from users import user56 as user_file56
 from users import user58 as user_file58
 
-# from users import user3 as user_file3
-# from users import user8 as user_file8
-# from users import user22 as user_file22
-# from users import user23 as user_file23
-# from users import user25 as user_file25
-# from users import user30 as user_file30
-# from users import user32 as user_file32
-# from users import user33 as user_file33
-# from users import user41 as user_file41
-# from users import user43 as user_file43
-# from users import user46 as user_file46
-# from users import user48 as user_file48
-# from users import user51 as user_file51
-# from users import user57 as user_file57
+from users import user3 as user_file3
+from users import user8 as user_file8
+from users import user22 as user_file22
+from users import user23 as user_file23
+from users import user25 as user_file25
+from users import user30 as user_file30
+from users import user32 as user_file32
+from users import user33 as user_file33
+from users import user41 as user_file41
+from users import user43 as user_file43
+from users import user46 as user_file46
+from users import user48 as user_file48
+from users import user51 as user_file51
+from users import user57 as user_file57
 
 # Group 2 users ~ 25 total
 from users import user1 as user_file1
@@ -45,28 +45,28 @@ from users import user42 as user_file42
 from users import user47 as user_file47
 from users import user60 as user_file60
 
-# from users import user5 as user_file5
-# from users import user10 as user_file10
-# from users import user14 as user_file14
-# from users import user21 as user_file21
-# from users import user24 as user_file24
-# from users import user26 as user_file26
-# from users import user31 as user_file31
-# from users import user39 as user_file39
-# from users import user44 as user_file44
-# from users import user45 as user_file45
-# from users import user52 as user_file52
-# from users import user53 as user_file53
-# from users import user55 as user_file55
-# from users import user59 as user_file59
-# from users import user61 as user_file61
+from users import user5 as user_file5
+from users import user10 as user_file10
+from users import user14 as user_file14
+from users import user21 as user_file21
+from users import user24 as user_file24
+from users import user26 as user_file26
+from users import user31 as user_file31
+from users import user39 as user_file39
+from users import user44 as user_file44
+from users import user45 as user_file45
+from users import user52 as user_file52
+from users import user53 as user_file53
+from users import user55 as user_file55
+from users import user59 as user_file59
+from users import user61 as user_file61
 
 def metricTracker(user_file, itr, group, user):
     # Set up MongoDB
     myclient = pymongo.MongoClient("mongodb+srv://khangwen197:HsUFKqdVgto1Qyo5@cluster0.pqitser.mongodb.net/")
     mydb = myclient["CSE4200"]
 
-    mycol = mydb["Iteration3_Metrics"]
+    mycol = mydb["Iteration4_Metrics"]
 
     # Initialize browser
     driver = webdriver.Firefox()
@@ -100,12 +100,13 @@ def metricTracker(user_file, itr, group, user):
     print(df_metric_data.sort_values(by=["Group", "User"]))
 
 if __name__ == "__main__":
-    group1 = "Control"  
-    group2 = "Test"
-    iteration = 3
+    group1 = "Final"  
+    group2 = "Final"
+    group3 = "Final"
+    iteration = 4
 
-    metricTracker(user_file2, iteration, group1, "User2")
-    metricTracker(user_file6, iteration, group1, "User6")
+    #metricTracker(user_file2, iteration, group1, "User2")
+    #metricTracker(user_file6, iteration, group1, "User6")
     metricTracker(user_file7, iteration, group1, "User7")
     metricTracker(user_file13, iteration, group1, "User13")
     metricTracker(user_file17, iteration, group1, "User17")
@@ -115,13 +116,45 @@ if __name__ == "__main__":
     metricTracker(user_file56, iteration, group1, "User56")
     metricTracker(user_file58, iteration, group1, "User58")
 
-    metricTracker(user_file1, iteration, group2, "User1")
-    metricTracker(user_file12, iteration, group2, "User12")
-    metricTracker(user_file15, iteration, group2, "User15")
-    metricTracker(user_file16, iteration, group2, "User16")
-    metricTracker(user_file34, iteration, group2, "User34")
-    metricTracker(user_file35, iteration, group2, "User35")
-    metricTracker(user_file40, iteration, group2, "User40")
-    metricTracker(user_file42, iteration, group2, "User42")
-    metricTracker(user_file47, iteration, group2, "User47")
-    metricTracker(user_file60, iteration, group2, "User60")
+    # metricTracker(user_file3, iteration, group1, "User3")
+    # metricTracker(user_file8, iteration, group1, "User8")
+    # metricTracker(user_file22, iteration, group1, "User22")
+    # metricTracker(user_file23, iteration, group1, "User23")
+    # metricTracker(user_file25, iteration, group1, "User25")
+    # metricTracker(user_file30, iteration, group1, "User30")
+    # metricTracker(user_file32, iteration, group1, "User32")
+    # metricTracker(user_file33, iteration, group1, "User33")
+    # metricTracker(user_file41, iteration, group1, "User41")
+    # metricTracker(user_file43, iteration, group1, "User43")
+    # metricTracker(user_file46, iteration, group1, "User46")
+    # metricTracker(user_file48, iteration, group1, "User48")
+    # metricTracker(user_file51, iteration, group1, "User51")
+    # metricTracker(user_file57, iteration, group1, "User57")
+
+    # metricTracker(user_file1, iteration, group2, "User1")
+    # metricTracker(user_file12, iteration, group2, "User12")
+    # metricTracker(user_file15, iteration, group2, "User15")
+    # metricTracker(user_file16, iteration, group2, "User16")
+    # metricTracker(user_file34, iteration, group2, "User34")
+    # metricTracker(user_file35, iteration, group2, "User35")
+    # metricTracker(user_file40, iteration, group2, "User40")
+    # metricTracker(user_file42, iteration, group2, "User42")
+    # metricTracker(user_file47, iteration, group2, "User47")
+    # metricTracker(user_file60, iteration, group2, "User60")
+
+    # metricTracker(user_file5, iteration, group2, "User5")
+    # metricTracker(user_file10, iteration, group2, "User10")
+    # metricTracker(user_file14, iteration, group2, "User14")
+    # metricTracker(user_file21, iteration, group2, "User21")
+    # metricTracker(user_file24, iteration, group2, "User24")
+    # metricTracker(user_file26, iteration, group2, "User26")
+    # metricTracker(user_file31, iteration, group2, "User31")
+    # metricTracker(user_file39, iteration, group2, "User39")
+    # metricTracker(user_file44, iteration, group2, "User44")
+    # metricTracker(user_file45, iteration, group2, "User45")
+    # metricTracker(user_file52, iteration, group2, "User52")
+    # metricTracker(user_file53, iteration, group2, "User53")
+    # metricTracker(user_file55, iteration, group2, "User55")
+    # metricTracker(user_file59, iteration, group2, "User59")
+    # metricTracker(user_file61, iteration, group2, "User61")
+
