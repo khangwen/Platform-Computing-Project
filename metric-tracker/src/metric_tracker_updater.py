37fa329fcd4112b3c92aee6fc3b9d26169f3cb3b
Alan Nguyen <a_wen197@yahoo.com>
2024-05-13 19:03:46 -0700

    added updater file

---
+++ metric-tracker/src/metric_tracker_updater.py
@@ -0,0 +1,31 @@
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pymongo
+import pandas as pd
+
+# Set up MongoDB
+myclient = pymongo.MongoClient("mongodb+srv://khangwen197:HsUFKqdVgto1Qyo5@cluster0.pqitser.mongodb.net/")
+mydb = myclient["CSE4200"]
+
+mycol = mydb["Iteration2_Metrics"]
+
+myquery = {"Iteration": 0}
+newvalues = {"$set": {"Iteration": 2}}
+mycol.update_many(myquery, newvalues)
+
+# Print data from MongoDB as table
+print("\nData from MongoDB:")
+metric_data = [data for data in mycol.find()]
+df_metric_data = pd.DataFrame(metric_data)
+# df_metric_data.to_csv('out.csv', index=False)  
+print(df_metric_data.sort_values(by=["Group", "User"]))
+
+# Calculate average presence time
+control_group = df_metric_data[df_metric_data["Group"] == "Control"]
+test_group = df_metric_data[df_metric_data["Group"] == "Test"]
+control_avg = control_group["PRESENCE_TIME (SEC.)"].mean()
+test_avg = test_group["PRESENCE_TIME (SEC.)"].mean()
+print(f"\nAverage presence time for Control group: {control_avg} seconds")
+print(f"Average presence time for Test group: {test_avg} seconds")
